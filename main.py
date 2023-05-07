32db3296512e7b364f0f5ac2eeb1cb124f74136e
MisterChief53 <soderol3@hotmail.com>
2023-05-06 22:30:45 -0600

    added frontend

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return render_template('index.html')
+    return render_template('Any.html')
+
+@app.route('/landingPage')
+def landingPage():
+    return render_template('landingPage.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
